92e367ededce3898d77906556aa6d4c2372bc4d2
yugantx@gmail.com <yugantx@gmail.com>
2022-09-15 23:35:54 +0530

    login add

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
+
+function onLoginSubmit(e) {
+  e.preventDefault();
+  loginForm.classList.add("hidden");
+  const username = loginInput.value;
+  greeting.textContent = `Hello ${username}`;
+  greeting.classList.remove("hidden");
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
